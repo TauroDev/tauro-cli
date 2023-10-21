f190bb72bcde024d352dfbc915b40bf813c5a3fc
andresroa <AndresMore1423@hotmail.com>
2023-10-21 16:31:26 -0500

    config temporal deploy

--- lib/Runners/deploy.runner.ts
+++ lib/Runners/deploy.runner.ts
@@ -1,23 +1,34 @@
 import { Config } from "../Configurations";
 import { incrementVersion } from "../Utils";
-import PackageJson from "../../package.json"
+import PackageJson from "../../package.json";
 import path from "path";
-import fsExtra from "fs-extra"
+import fsExtra from "fs-extra";
+import execa from "execa";
 
-const routeConfig = path.join(__dirname, '../Configurations/params.config.json')
-const routePackage = path.join(__dirname, '../../package.json')
-const pathProject = path.join(__dirname, '../../Templates')
-const tmpVersion = Config.versionApp
+const initDeployConfig = async () => {
+    const { stdout } = await execa("git", ["rev-parse", "--abbrev-ref", "HEAD"], {
+        cwd: path.join(__dirname, "../../"),
+      });
+      const branch = stdout.trim();
+      console.log(branch)
+}
 
-const tmpConfig = Config
-const tmpPackage = PackageJson
-tmpConfig.versionApp = incrementVersion(tmpVersion)
-tmpPackage.version = incrementVersion(tmpVersion)
-tmpConfig.isNewFront = true
-tmpConfig.isNewBack = true
-tmpConfig.isProd = true
+initDeployConfig();
 
-fsExtra.writeFileSync(routeConfig, JSON.stringify(tmpConfig, null, 2));
-fsExtra.writeFileSync(routePackage, JSON.stringify(tmpPackage, null, 2));
-fsExtra.removeSync(`${pathProject}/base-front-end`)
-fsExtra.removeSync(`${pathProject}/base-back-end`)
+// const routeConfig = path.join(__dirname, '../Configurations/params.config.json')
+// const routePackage = path.join(__dirname, '../../package.json')
+// const pathProject = path.join(__dirname, '../../Templates')
+// const tmpVersion = Config.versionApp
+
+// const tmpConfig = Config
+// const tmpPackage = PackageJson
+// tmpConfig.versionApp = incrementVersion(tmpVersion)
+// tmpPackage.version = incrementVersion(tmpVersion)
+// tmpConfig.isNewFront = true
+// tmpConfig.isNewBack = true
+// tmpConfig.isProd = true
+
+// fsExtra.writeFileSync(routeConfig, JSON.stringify(tmpConfig, null, 2));
+// fsExtra.writeFileSync(routePackage, JSON.stringify(tmpPackage, null, 2));
+// fsExtra.removeSync(`${pathProject}/base-front-end`)
+// fsExtra.removeSync(`${pathProject}/base-back-end`)
