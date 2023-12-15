354e8d96da02b806b58171e16ed2161efdd9902a
Andres Roa <andresmore1423@hotmail.com>
2023-12-15 16:06:17 -0500

    custom button

--- lib/Utils/genericPages.ts
+++ lib/Utils/genericPages.ts
@@ -3,7 +3,7 @@ import prettier from "prettier";
 export const GenericPage = async (name: string) => {
   const contentFile = `
     import React from 'react'
-    import styles from "./${name}style.module.css"
+    import styles from "./${name}.style.module.css"
     import ${name}ViewModel from "./${name}.viewmodel"
 
     const ${name} = () => {
@@ -11,7 +11,7 @@ export const GenericPage = async (name: string) => {
         return (
             <div className={styles.content}>
                 <h1>Página ${name} creada por @tauro/cli</h1>
-                <button onClick={Increment}>Número actual: {count}</button>
+                <button className={styles.button} onClick={Increment}>Número actual: {count}</button>
             </div>
         )
     }
@@ -33,6 +33,31 @@ export const GenericPageStyle = async () => {
         justify-content: center;
         align-items: center;
         flex-direction: column;
+    }
+
+    .button {
+      padding: 10px 20px;
+      font-size: 16px;
+      font-weight: light; 
+      color: #eceff4; 
+      background: #1b1b1b; 
+      border: none; 
+      border-radius: 25px;
+      cursor: pointer;
+      outline: none;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+      transition: background-color 0.3s, box-shadow 0.3s, transform 0.3s;
+    }
+    
+    .button:hover {
+      background: #353535;
+      box-shadow: 0 6px 8px rgba(0, 0, 0, 0.2);
+      transform: translateY(-2px);
+    }
+    
+    .button:active {
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+      transform: translateY(1px);
     }
 `;
   const formattedCode = await prettier.format(contentFile, {
